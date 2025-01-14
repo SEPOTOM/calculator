bc6f2496e9242c0b11e8b3375dfd1223902011e0
Artyom Pizin <artempetr2016@gmail.com>
2025-01-14 13:20:37 +0300

    feat(App): display digit of clicked digit button on display

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,74 @@
-const App = () => (
-  <>
-    <input role="alert" type="text" readOnly aria-label="Calculator display" />
-    <button type="button">0</button>
-    <button type="button">1</button>
-    <button type="button">2</button>
-    <button type="button">3</button>
-    <button type="button">4</button>
-    <button type="button">5</button>
-    <button type="button">6</button>
-    <button type="button">7</button>
-    <button type="button">8</button>
-    <button type="button">9</button>
-    <button type="button" aria-label="plus">
-      +
-    </button>
-    <button type="button" aria-label="minus">
-      -
-    </button>
-    <button type="button" aria-label="divide">
-      /
-    </button>
-    <button type="button" aria-label="multiply">
-      *
-    </button>
-    <button type="button" aria-label="equal">
-      =
-    </button>
-    <button type="button" aria-label="clear">
-      C
-    </button>
-    <button type="button" aria-label="clear all">
-      AC
-    </button>
-  </>
-);
+import { MouseEvent, useState } from 'react';
+
+const App = () => {
+  const [displayValue, setDisplayValue] = useState('');
+
+  const handleDigitButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
+    setDisplayValue(e.currentTarget.textContent ?? '');
+  };
+
+  return (
+    <>
+      <input
+        role="alert"
+        type="text"
+        value={displayValue}
+        readOnly
+        aria-label="Calculator display"
+      />
+      <button type="button" onClick={handleDigitButtonClick}>
+        0
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        1
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        2
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        3
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        4
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        5
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        6
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        7
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        8
+      </button>
+      <button type="button" onClick={handleDigitButtonClick}>
+        9
+      </button>
+      <button type="button" aria-label="plus">
+        +
+      </button>
+      <button type="button" aria-label="minus">
+        -
+      </button>
+      <button type="button" aria-label="divide">
+        /
+      </button>
+      <button type="button" aria-label="multiply">
+        *
+      </button>
+      <button type="button" aria-label="equal">
+        =
+      </button>
+      <button type="button" aria-label="clear">
+        C
+      </button>
+      <button type="button" aria-label="clear all">
+        AC
+      </button>
+    </>
+  );
+};
 
 export default App;
