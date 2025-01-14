5e56b0f46e8f8ef219b0c29cc40b1d69a9781fd5
Artyom Pizin <artempetr2016@gmail.com>
2025-01-14 11:15:37 +0300

    init: start development

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  experimentalTernaries: true,
+  singleQuote: true,
+  importOrder: ['^react(.*)$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+};
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+
+import viteConfig from './vite.config.ts';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setupTests.ts',
+      passWithNoTests: true,
+      dir: './src',
+    },
+  }),
+);
