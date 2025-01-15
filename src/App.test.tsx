6ca77ec7f9657a765e516e231d6ad4471d8532c4
Artyom Pizin <artempetr2016@gmail.com>
2025-01-15 10:35:02 +0300

    test(App): add test for erasing prev number/result after clear all btn click

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -188,4 +188,20 @@ describe('clear all button', () => {
       '0',
     );
   });
+
+  it('should erase the previous number/result', async () => {
+    const { user } = renderWithUser(<App />);
+
+    await user.click(screen.getByRole('button', { name: '9' }));
+    await user.click(screen.getByRole('button', { name: 'plus' }));
+
+    await user.click(screen.getByRole('button', { name: 'clear all' }));
+
+    await user.click(screen.getByRole('button', { name: '3' }));
+    await user.click(screen.getByRole('button', { name: 'equal' }));
+
+    expect(screen.getByRole('alert', { name: /display/i })).toHaveDisplayValue(
+      '3',
+    );
+  });
 });
