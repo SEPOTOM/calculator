1fafb41bc5b3684b3b793bd791a582c74b726493
Artyom Pizin <artempetr2016@gmail.com>
2025-01-15 11:00:30 +0300

    test(App): add tests for changing sign of entered num after change sign btn click

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -121,6 +121,37 @@ describe('App', () => {
       screen.getByRole('button', { name: "change number's sign" }),
     ).toHaveTextContent('+/-');
   });
+
+  describe('should change the sign of the entered number after a change sign button click', () => {
+    it('positive to negative', async () => {
+      const { user } = renderWithUser(<App />);
+      await user.click(screen.getByRole('button', { name: '5' }));
+
+      await user.click(
+        screen.getByRole('button', { name: "change number's sign" }),
+      );
+
+      expect(
+        screen.getByRole('alert', { name: /display/i }),
+      ).toHaveDisplayValue('-5');
+    });
+
+    it('positive to negative', async () => {
+      const { user } = renderWithUser(<App />);
+      await user.click(screen.getByRole('button', { name: '1' }));
+
+      await user.click(
+        screen.getByRole('button', { name: "change number's sign" }),
+      );
+      await user.click(
+        screen.getByRole('button', { name: "change number's sign" }),
+      );
+
+      expect(
+        screen.getByRole('alert', { name: /display/i }),
+      ).toHaveDisplayValue('1');
+    });
+  });
 });
 
 describe('operations', () => {
