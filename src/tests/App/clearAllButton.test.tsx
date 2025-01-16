5f7ef3ceb98c0cbcd690fcda4af90a77de5d12bb
Artyom Pizin <artempetr2016@gmail.com>
2025-01-16 13:03:11 +0300

    test(clearAllButton): improve readability

--- src/tests/App/clearAllButton.test.tsx
+++ src/tests/App/clearAllButton.test.tsx
@@ -10,8 +10,8 @@ import {
 describe('Clear all button', () => {
   it('should reset display to 0', async () => {
     const { user } = renderWithUser(<App />);
-
     await enterNumber(user, '83');
+
     await clearAll(user);
 
     expectDisplayValueToBe('0');
@@ -19,9 +19,9 @@ describe('Clear all button', () => {
 
   it("should clear app's history", async () => {
     const { user } = renderWithUser(<App />);
-
     await enterNumber(user, '9');
     await clickButtons(user, ['plus']);
+
     await clearAll(user);
     await enterNumber(user, '3');
     await clickButtons(user, ['equal']);
