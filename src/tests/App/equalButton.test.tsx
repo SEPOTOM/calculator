213a0bf8b3317b46958bf0c8f3affd8a5afd956a
Artyom Pizin <artempetr2016@gmail.com>
2025-01-16 13:04:43 +0300

    test(equalButton): improve readability

--- src/tests/App/equalButton.test.tsx
+++ src/tests/App/equalButton.test.tsx
@@ -9,8 +9,8 @@ import {
 describe('Equal button', () => {
   it('should maintain display value when no operation is pending', async () => {
     const { user } = renderWithUser(<App />);
-
     await enterNumber(user, '9');
+
     await clickButtons(user, ['equal']);
 
     expectDisplayValueToBe('9');
